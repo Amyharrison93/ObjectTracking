d0947b247be54c6137626cf070726884cffa0dbf
Amy Harrison <amycantflynow@gmail.com>
2020-05-22 10:59:51 +0100

    added camera read

--- ObjectTracking/ObjectTracking/ObjectTracking.py
+++ ObjectTracking/ObjectTracking/ObjectTracking.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 import numpy as np
 import cv2 as cv 
+
+intCamLoc = 0
+try:
+    objCam = cv.VideoCapture(intCamLoc, cv.CAP_DSHOW)
+except:
+    objCam = "Error: Camera not found"
+    print(objCam)
+
+while (objCam != "Error: Camera not found"):
+
+    #capture image
+    state, arryFrame = objCam.read()
+    #reduce image
+    arryFrame = 
